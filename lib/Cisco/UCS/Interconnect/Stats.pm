79e2b9446d23a8ed157ace9c341b5423cff251f6
Interactive Administrative User <intadmin@bsgfpt02.interactive.com.au>
2014-06-23 16:05:06 +1000

    Added new class Cisco::UCS::Interconnect::Stats

---
+++ lib/Cisco/UCS/Interconnect/Stats.pm
@@ -0,0 +1,48 @@
+package Cisco::UCS::Interconnect::Stats;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(weaken);
+use Carp qw(croak);
+
+our $VERSION    = '0.1';
+
+our %ATTRIBUTES = (
+                        load		=> 'load',
+                        load_avg	=> 'loadAvg',
+                        load_min	=> 'loadMin',
+                        load_max	=> 'loadMax',
+                        mem_available	=> 'memAvailable',
+                        mem_available_avg=> 'memAvailableAvg',
+                        mem_available_min=> 'memAvailableMin',
+                        mem_available_max=> 'memAvailableMax',
+                        mem_cached	=> 'memCached',
+                        mem_cached_avg	=> 'memCachedAvg',
+                        mem_cached_min	=> 'memCachedMin',
+                        mem_cached_max	=> 'memCachedMax',
+                        suspect         => 'oobIfIp',
+                );
+
+{ no strict 'refs';
+
+        while ( my ($attribute, $pseudo) = each %ATTRIBUTES ) {
+                *{ __PACKAGE__ .'::'. $pseudo } = sub {
+                        my $self = shift;
+                        return $self->{$attribute}
+                }
+        }
+}
+
+sub new {
+        my ( $class, $args ) = @_;
+        my $self = {};
+        bless $self, $class;
+
+        while ( my( $k, $v ) = each %{ $args->{outConfig}->{swSystemStats} } ) { $self->{$k} = $v }
+
+        return $self;
+}
+
+
+1;
